2228c8abfaf24337c6d56fa1a805b22fdc7cfdf7
mdshakibsami <mdshakibsami21@gmail.com>
2025-08-11 01:12:21 +0600

    even odd

---
+++ 8 Even Odd/1.asm
@@ -0,0 +1,36 @@
+.model small
+.stack 100h
+.data      
+str1 dw 'Enter number(0-9): $'
+even_msg db 0ah, 0dh,'Result is: Even$'
+odd_msg db 0ah, 0dh,'Result is: Odd$'    
+num dw ?
+.code
+main:
+    mov ax, @data
+    mov ds, ax    
+    
+    mov ah, 09h
+    lea dx, str1
+    int 21h  
+    
+    mov ah, 01h
+    int 21h   
+    
+    mov num, ax
+    and num, 1
+    jz even
+
+    lea dx, odd_msg
+    jmp print
+
+even:
+    lea dx, even_msg
+
+print:
+    mov ah, 09h
+    int 21h
+
+    mov ah, 4ch
+    int 21h
+end main
